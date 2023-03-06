54c846428e896058444d77c511089e9cf775dd5a
mistergama <marcelo.allan@gmail.com>
2023-03-06 19:14:10 -0300

    HouseController - store - upload de imagem

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import path from 'path';
 import routes from './router';
 
-class App{
+class App {
 	constructor() {
 		this.server = express();
 		mongoose.connect('mongodb+srv://marceloallan:marcelogama@devhouse.mmhldej.mongodb.net/?retryWrites=true&w=majority', {
@@ -13,17 +13,21 @@ class App{
 		this.middlewares();
 		this.routes();
 	}
+
 	middlewares() {
+		this.server.use(
+			'/files',
+			express.static(path.resolve(__dirname, '..', 'uploads')),
+		);
 		this.server.use(express.json());
-		
 	}
 
 	routes() {
 		this.server.use(routes);
 	}
+
 }
 
 export default new App().server;
-
 
 
